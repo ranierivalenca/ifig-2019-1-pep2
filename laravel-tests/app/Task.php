2975a55b4a5f22f74eaa94ba3e551f6fa0492ff6
Ranieri <ranierivalenca@gmail.com>
2019-03-28 14:49:32 -0300

    Adicionando campos ao model.
    Tipos de colunas disponíveis no laravel: https://laravel.com/docs/5.8/migrations#creating-columns
    
    Definição de models no laravel: https://laravel.com/docs/5.8/eloquent#defining-models

--- laravel-tests/app/Task.php
+++ laravel-tests/app/Task.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     //
+    protected $fillable = [
+        'title',
+        'description',
+        'expires',
+    ];
 }
--- laravel-tests/database/migrations/2019_03_28_173933_create_tasks_table.php
+++ laravel-tests/database/migrations/2019_03_28_173933_create_tasks_table.php
@@ -15,6 +15,9 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('description');
+            $table->date('expires');
             $table->timestamps();
         });
     }
