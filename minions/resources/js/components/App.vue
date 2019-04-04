6ad55fe1a7dc52b2788a98be7c5b76c6c4216b5f
Ranieri <ranierivalenca@gmail.com>
2019-04-04 13:08:18 -0300

    #1 - Esqueleto da aplicação vue

---
+++ minions/resources/js/components/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <div class="heading">
+      <h1>Minions</h1>
+    </div>
+    <minion-component
+      v-for="minion in minions"
+      v-bind="minion"
+      :key="minion.id"
+      @update="update"
+      @delete="del"
+    ></minion-component>
+    <div>
+      <button @click="create()">Add</button>
+    </div>
+  </div>
+</template>
+<script>
+  function Minion({ id, color, name}) {
+    this.id = id;
+    this.color = color;
+    this.name = name;
+  }
+
+  import MinionComponent from './MinionComponent.vue';
+
+  export default {
+    data() {
+      return {
+        minions: []
+      }
+    },
+    methods: {
+      create() {
+        // To do
+      },
+      read() {
+        // To do
+      },
+      update(id, color) {
+        // To do
+      },
+      del(id) {
+        // To do
+      }
+    },
+    components: {
+      MinionComponent
+    }
+  }
+</script>
